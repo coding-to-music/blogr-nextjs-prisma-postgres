45ad817de4d131740c09025f377d3ea4d63f599b
coding-to-music <connors.tom@gmail.com>
2022-05-16 01:54:17 +0000

    Build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,18 @@
 
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
-
+import FingerprintJS from "@fingerprintjs/fingerprintjs-pro";
 import LogRocket from "logrocket";
+
 LogRocket.init("zas8ib/blogr-nextjs-prisma-postgres");
+
+// Initialize an agent at application startup.
+const fpPromise = FingerprintJS.load({ apiKey: "o6o11R6yNNN96RpBRfZ2" });
+
+// Get the visitor identifier when you need it.
+fpPromise
+  .then((fp) => fp.get())
+  .then((result) => console.log(result.visitorId));
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
