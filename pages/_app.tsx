6034dff0bb12e659b52825460c5f73e7be35d8fc
coding-to-music <connors.tom@gmail.com>
2022-05-16 01:44:41 +0000

    Build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,9 @@
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 
+import LogRocket from "logrocket";
+LogRocket.init("zas8ib/blogr-nextjs-prisma-postgres");
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
