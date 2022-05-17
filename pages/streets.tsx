358000605bd01b1d5003e7f8109a0f1d45510e29
coding-to-music <connors.tom@gmail.com>
2022-05-17 05:59:56 +0000

    Build

--- pages/streets.tsx
+++ pages/streets.tsx
@@ -6,7 +6,9 @@ import useSWR from "swr";
 //Write a fetcher function to wrap the native fetch function and return the result of a call to url in json format
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
-export default function Index() {
+// export default function Index() {
+
+const Streetdata: React.FC<Props> = (props) => {
   //Set up SWR to run the fetcher function when calling "/api/staticdata"
   //There are 3 possible states: (1) loading when data is null (2) ready when the data is returned (3) error when there was an error fetching the data
   const { data, error } = useSWR("/api/streetdata", fetcher);
@@ -17,21 +19,61 @@ export default function Index() {
   if (!data) return <div>Loading...</div>;
   //Handle the ready state and display the result contained in the data object mapped to the structure of the json file
   return (
-    <div>
-      <h1>JSON from streets.json file</h1>
-      <ul>
-        <li>name: {data.record.name}</li>
-        <li>from: {data.record.from}</li>
-        <li>to: {data.record.to}</li>
-        <li>width: {data.record.width}</li>
-        <li>length: {data.record.length}</li>
-        <li>date: {data.record.date}</li>
-        <li>non-city: {data.record.noncity}</li>
-        <li>unnaccepted-length: {data.record.unnacceptedlength}</li>
-      </ul>
-    </div>
+    <Layout>
+      <div className="page">
+        <h1>Streetdata</h1>
+        <main>
+          <ul>
+            <li>name: {data.record.name}</li>
+            <li>length: {data.record.length}</li>
+          </ul>
+        </main>
+      </div>
+      <div>
+        <h1>JSON from streets.json file</h1>
+        <ul>
+          <li>name: {data.record.name}</li>
+          <li>from: {data.record.from}</li>
+          <li>to: {data.record.to}</li>
+          <li>width: {data.record.width}</li>
+          <li>length: {data.record.length}</li>
+          <li>date: {data.record.date}</li>
+          <li>non-city: {data.record.noncity}</li>
+          <li>unnaccepted-length: {data.record.unnacceptedlength}</li>
+        </ul>
+      </div>
+
+      <style jsx>{`
+        .post {
+          background: var(--geist-background);
+          transition: box-shadow 0.1s ease-in;
+        }
+
+        .post:hover {
+          box-shadow: 1px 1px 3px #aaa;
+        }
+
+        .post + .post {
+          margin-top: 2rem;
+        }
+      `}</style>
+    </Layout>
   );
-}
+};
+
+export default Streetdata;
+
+// <div className="page">
+// <h1>Streetdata</h1>
+// <main>
+//   {props.drafts.map((post) => (
+//     <div key={post.id} className="post">
+//       <Post post={post} />
+//      </div>
+//   ))}
+// </main>
+
+// </div>
 
 // Sample Data
 
