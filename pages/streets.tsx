6f2327bb637dca652b7cc4fe30904d5b2c45ce72
coding-to-music <connors.tom@gmail.com>
2022-05-17 02:54:58 +0000

    Build

--- pages/streets.tsx
+++ pages/streets.tsx
@@ -9,7 +9,7 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 export default function Index() {
   //Set up SWR to run the fetcher function when calling "/api/staticdata"
   //There are 3 possible states: (1) loading when data is null (2) ready when the data is returned (3) error when there was an error fetching the data
-  const { data, error } = useSWR("/api/staticdata", fetcher);
+  const { data, error } = useSWR("/api/streetdata", fetcher);
 
   //Handle the error state
   if (error) return <div>Failed to load</div>;
