13ed63c34f093b9d8fe52329599c4f7477e7bcd1
coding-to-music <connors.tom@gmail.com>
2022-05-20 08:23:13 +0000

    Build

--- components/Street.tsx
+++ components/Street.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { Key } from "react";
 import Router from "next/router";
 // import ReactMarkdown from "react-markdown";
 
+// model Street {
+//   id                Int       @id @default(autoincrement())
+//   name              String?
+//   from              String?
+//   to                String?
+//   width             String?
+//   length            String?
+//   date              String?
+//   noncity           String?
+//   unnacceptedlength String?
+//   area              Int?      @default(0)
+//   createdAt         DateTime  @default(now()) @map(name: "created_at")
+//   updatedAt         DateTime  @default(now()) @map(name: "updated_at")
+//   @@map(name: "street")
+// }
+
+// export type PostProps = {
+//   id: string;
+//   title: string;
+//   author: {
+//     name: string;
+//     email: string;
+//   } | null;
+//   content: string;
+//   published: boolean;
+// };
+
 export type StreetProps = {
-  id: String;
+  id: Key;
   name: String;
   from: String;
   to: String;
@@ -12,6 +39,9 @@ export type StreetProps = {
   date: String;
   noncity: String;
   unnacceptedlength: String;
+  area: Number;
+  createdAt: Date;
+  updatedAt: Date;
 };
 
 const Street: React.FC<{ street: StreetProps }> = ({ street }) => {
